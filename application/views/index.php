c61df02b3151bf1d40c188c2d47dc4ee2168e3d4
raymondfy14 <raymond.faraz105919@students.unila.ac.id>
2021-07-12 22:51:54 +0700

    Web GIS Part 1

---
+++ application/views/index.php
@@ -0,0 +1,15 @@
+
+
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div id="mapid" style="width: 600px; height: 400px;"></div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
